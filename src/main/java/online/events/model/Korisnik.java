64fc1f8e34796fec5cfef4708fb5b26d18658a59
Rudy <tomica.korpar@gmail.com>
2021-01-27 20:51:48 +0100

    entity class

---
+++ src/main/java/online/events/model/Korisnik.java
@@ -0,0 +1,104 @@
+package online.events.model;
+
+import javax.persistence.*;
+
+/**
+ *
+ * Korisnik entity class
+ *
+ */
+@Entity
+@Table(name = "korisnik", schema = "igea_tk")
+public class Korisnik {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sifra", nullable = false)
+    private Integer sifra;
+
+    @Column(name = "korisnicko_ime", nullable = false, length = 50)
+    private String korisnicko_ime;
+
+    @Column(name = "ime", nullable = false, length = 50)
+    private String ime;
+
+    @Column(name = "prezime", nullable = false, length = 50)
+    private String prezime;
+
+    @Column(name = "oib", nullable = false, length = 11)
+    private String oib;
+
+    @Column(name = "email", nullable = false, length = 11)
+    private String email;
+
+
+
+    @JoinColumn(name = "tip", referencedColumnName = "sifra")
+    @ManyToOne
+    private TipKorisnika tipKorisnika;
+
+    //constructors
+    public Korisnik() {
+        super();
+    }
+
+    //getters & setters
+
+    public Integer getSifra() {
+        return sifra;
+    }
+
+    public void setSifra(Integer sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getKorisnicko_ime() {
+        return korisnicko_ime;
+    }
+
+    public void setKorisnicko_ime(String korisnicko_ime) {
+        this.korisnicko_ime = korisnicko_ime;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getOib() {
+        return oib;
+    }
+
+    public void setOib(String oib) {
+        this.oib = oib;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public TipKorisnika getTipKorisnika() {
+        return tipKorisnika;
+    }
+
+    public void setTipKorisnika(TipKorisnika tipKorisnika) {
+        this.tipKorisnika = tipKorisnika;
+    }
+}
+
+
---
+++ src/main/java/online/events/model/TipKorisnika.java
@@ -0,0 +1,59 @@
+package online.events.model;
+
+import javax.persistence.*;
+
+/**
+ *
+ * Tip korisnika entity class
+ *
+ */
+@Entity
+@Table(name = "tip_korisnika", schema = "igea_tk")
+public class TipKorisnika {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sifra", nullable = false)
+    private Integer sifraTipKorisnika;
+
+    @Column(name = "naziv", nullable = true, length = 50)
+    private String nazivTipKorisnika;
+
+    @Column(name = "aktivan", nullable = false)
+    private Boolean aktivanTipKorisnika;
+
+
+    //constructors
+    public TipKorisnika() {
+        super();
+    }
+
+    //getters & setters
+
+
+    public Integer getSifraTipKorisnika() {
+        return sifraTipKorisnika;
+    }
+
+    public void setSifraTipKorisnika(Integer sifraTipKorisnika) {
+        this.sifraTipKorisnika = sifraTipKorisnika;
+    }
+
+    public String getNazivTipKorisnika() {
+        return nazivTipKorisnika;
+    }
+
+    public void setNazivTipKorisnika(String nazivTipKorisnika) {
+        this.nazivTipKorisnika = nazivTipKorisnika;
+    }
+
+    public Boolean getAktivanTipKorisnika() {
+        return aktivanTipKorisnika;
+    }
+
+    public void setAktivanTipKorisnika(Boolean aktivanTipKorisnika) {
+        this.aktivanTipKorisnika = aktivanTipKorisnika;
+    }
+}
+
+
