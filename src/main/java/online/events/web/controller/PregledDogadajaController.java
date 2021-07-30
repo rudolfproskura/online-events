a55036a02b48494c2a3c575fd9e1a0178f668cf8
Rudy <tomica.korpar@gmail.com>
2021-07-30 12:08:08 +0200

    pregled svih dogadaja

--- src/main/java/online/events/web/controller/PregledDogadajaController.java
+++ src/main/java/online/events/web/controller/PregledDogadajaController.java
@@ -1,39 +1,42 @@
 package online.events.web.controller;
 
-import online.events.dao.ExtenderService;
+import online.events.dao.DogadajDao;
+import online.events.dto.DogadajDto;
+import online.events.dto.DogadajFilterDto;
+import online.events.exception.DogadajAppRuleException;
+import online.events.util.DogadajAppConstants;
+import org.apache.commons.lang3.StringUtils;
 import org.primefaces.event.ScheduleEntryMoveEvent;
 import org.primefaces.event.ScheduleEntryResizeEvent;
 import org.primefaces.event.SelectEvent;
-import org.primefaces.model.*;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.event.AjaxBehaviorEvent;
-import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Named
 @ViewScoped
 public class PregledDogadajaController implements Serializable {
 
     @Inject
-    private ExtenderService extenderService;
+    private DogadajDao dogadajDao;
 
     private ScheduleModel eventModel;
 
-    private ScheduleModel lazyEventModel;
-
     private ScheduleEvent<?> event = new DefaultScheduleEvent<>();
+
+    private List<DogadajDto> dogadajiFilterList;
+    private DogadajFilterDto dogadajFilterDto;
 
     private boolean slotEventOverlap = true;
     private boolean showWeekNumbers = false;
@@ -71,161 +74,72 @@ public class PregledDogadajaController implements Serializable {
     private String extenderCode = "// Write your code here or select an example from above";
     private String selectedExtenderExample = "";
 
-    private Map<String, ExtenderService.ExtenderExample> extenderExamples;
 
     @PostConstruct
     public void init() {
+        dogadajFilterDto = new DogadajFilterDto();
         eventModel = new DefaultScheduleModel();
+        getListDogadaj();
 
-        DefaultScheduleEvent<?> event = DefaultScheduleEvent.builder()
-                .title("Nogometna utakmica")
-                .startDate(LocalDateTime.now())
-                .endDate(LocalDateTime.now().plusHours(2))
-                .description("Varaždin.............. ulaz slobodan")
-                .borderColor("blue")
-                .build();
-        eventModel.addEvent(event);
+        for (DogadajDto dogadajDto : dogadajiFilterList) {
+            DefaultScheduleEvent<?> event = DefaultScheduleEvent.builder()
+                    .title(dogadajDto.getNazivDogadaja())
+                    .startDate(dogadajDto.getVrijemeOd())
+                    .endDate(dogadajDto.getVrijemeDo())
+                    .description(dogadajDto.getGradDogadajaDto().getNazivGrada())
+                    .borderColor((StringUtils.equals(dogadajDto.getSlobodanUlaz(), "DA") ? "GREEN" : "BLUE"))
+                    .overlapAllowed(true)
+                    .build();
+            eventModel.addEvent(event);
+        }
 
-        event = DefaultScheduleEvent.builder()
-                .title("Champions League Match")
-                .startDate(previousDay8Pm())
-                .endDate(previousDay11Pm())
-                .description("Team A vs. Team B")
-                .url("https://www.uefa.com/uefachampionsleague/")
-                .borderColor("orange")
-                .build();
-        eventModel.addEvent(event);
 
-        event = DefaultScheduleEvent.builder()
-                .startDate(LocalDateTime.now().minusDays(6))
-                .endDate(LocalDateTime.now().minusDays(3))
-                .overlapAllowed(true)
-                .editable(false)
-                .resizable(false)
-                .display(ScheduleDisplayMode.BACKGROUND)
-                .backgroundColor("lightgreen")
-                .build();
-        eventModel.addEvent(event);
 
-        event = DefaultScheduleEvent.builder()
-                .title("Birthday Party")
-                .startDate(today1Pm())
-                .endDate(today6Pm())
-                .description("Aragon")
-                .overlapAllowed(true)
-                .borderColor("#CB4335")
-                .build();
-        eventModel.addEvent(event);
+    }
 
-        event = DefaultScheduleEvent.builder()
-                .title("Breakfast at Tiffanys (always resizable)")
-                .startDate(nextDay9Am())
-                .endDate(nextDay11Am())
-                .description("all you can eat")
-                .overlapAllowed(true)
-                .resizable(true)
-                .borderColor("#27AE60")
-                .build();
-        eventModel.addEvent(event);
 
-        event = DefaultScheduleEvent.builder()
-                .title("Plant the new garden stuff (always draggable)")
-                .startDate(theDayAfter3Pm())
-                .endDate(fourDaysLater3pm())
-                .description("Trees, flowers, ...")
-                .draggable(true)
-                .borderColor("#27AE60")
-                .build();
-        eventModel.addEvent(event);
-
-        DefaultScheduleEvent<?> scheduleEventAllDay = DefaultScheduleEvent.builder()
-                .title("Holidays (AllDay)")
-                .startDate(sevenDaysLater0am())
-                .endDate(eightDaysLater0am())
-                .description("sleep as long as you want")
-                .borderColor("#27AE60")
-                .allDay(true)
-                .build();
-        eventModel.addEvent(scheduleEventAllDay);
-
-        lazyEventModel = new LazyScheduleModel() {
-
-            @Override
-            public void loadEvents(LocalDateTime start, LocalDateTime end) {
-                for (int i = 1; i <= 5; i++) {
-                    LocalDateTime random = getRandomDateTime(start);
-                    addEvent(DefaultScheduleEvent.builder().title("Lazy Event " + i).startDate(random).endDate(random.plusHours(3)).build());
+    public void getListDogadaj() {
+        try {
+            dogadajiFilterList = dogadajDao.getFilterList(dogadajFilterDto);
+        } catch (DogadajAppRuleException eventEx) {
+            if (eventEx.getMessages() != null && !eventEx.getMessages().isEmpty()) {
+                for (String message : eventEx.getMessages()) {
+                    eventEx.printStackTrace();
+                    addMessage(message, DogadajAppConstants.SEVERITY_ERR);
                 }
             }
-        };
-
-    //    extenderExamples = extenderService.createExtenderExamples();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            addMessage("Došlo je do greške prilikom pretraživanja događaja.", DogadajAppConstants.SEVERITY_ERR);
+        }
     }
 
-    public ExtenderService getScheduleExtenderService() {
-        return extenderService;
+    public void addMessage(String summary, String severity) {
+        if (StringUtils.isNotBlank(summary) && StringUtils.isNotBlank(severity)) {
+            FacesMessage message = null;
+            switch (severity) {
+                case DogadajAppConstants.SEVERITY_ERR:
+                    message = new FacesMessage(FacesMessage.SEVERITY_ERROR, summary, null);
+                    break;
+                case DogadajAppConstants.SEVERITY_INFO:
+                    message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
+                    break;
+                case DogadajAppConstants.SEVERITY_WARN:
+                    message = new FacesMessage(FacesMessage.SEVERITY_WARN, summary, null);
+                    break;
+                default:
+                    message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, null);
+            }
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
     }
 
-    public void setScheduleExtenderService(ExtenderService extenderService) {
-        this.extenderService = extenderService;
-    }
 
-    public LocalDateTime getRandomDateTime(LocalDateTime base) {
-        LocalDateTime dateTime = base.withMinute(0).withSecond(0).withNano(0);
-        return dateTime.plusDays(((int) (Math.random() * 30)));
-    }
 
     public ScheduleModel getEventModel() {
         return eventModel;
     }
 
-    public ScheduleModel getLazyEventModel() {
-        return lazyEventModel;
-    }
-
-    private LocalDateTime previousDay8Pm() {
-        return LocalDateTime.now().minusDays(1).withHour(20).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime previousDay11Pm() {
-        return LocalDateTime.now().minusDays(1).withHour(23).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime today1Pm() {
-        return LocalDateTime.now().withHour(13).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime theDayAfter3Pm() {
-        return LocalDateTime.now().plusDays(1).withHour(15).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime today6Pm() {
-        return LocalDateTime.now().withHour(18).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime nextDay9Am() {
-        return LocalDateTime.now().plusDays(1).withHour(9).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime nextDay11Am() {
-        return LocalDateTime.now().plusDays(1).withHour(11).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime fourDaysLater3pm() {
-        return LocalDateTime.now().plusDays(4).withHour(15).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime sevenDaysLater0am() {
-        return LocalDateTime.now().plusDays(7).withHour(0).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    private LocalDateTime eightDaysLater0am() {
-        return LocalDateTime.now().plusDays(7).withHour(0).withMinute(0).withSecond(0).withNano(0);
-    }
-
-    public LocalDate getInitialDate() {
-        return LocalDate.now().plusDays(1);
-    }
 
     public ScheduleEvent<?> getEvent() {
         return event;
@@ -277,295 +191,11 @@ public class PregledDogadajaController implements Serializable {
         addMessage(message);
     }
 
-    public void onEventDelete() {
-        String eventId = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap().get("eventId");
-        if (event != null) {
-            ScheduleEvent<?> event = eventModel.getEvent(eventId);
-            eventModel.deleteEvent(event);
-        }
-    }
-
-    public void onExtenderExampleSelect(AjaxBehaviorEvent event) {
-        ExtenderService.ExtenderExample example = getExtenderExample();
-        if (!"custom".equals(selectedExtenderExample) && example != null) {
-            if (example.getDetails() != null && !example.getDetails().isEmpty()) {
-                FacesMessage message = new FacesMessage(example.getName(), example.getDetails());
-                FacesContext.getCurrentInstance().addMessage(event.getComponent().getClientId(), message);
-            }
-            this.extenderCode = example.getValue();
-        }
-    }
-
     private void addMessage(FacesMessage message) {
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
-    public boolean isShowWeekends() {
-        return showWeekends;
-    }
 
-    public void setShowWeekends(boolean showWeekends) {
-        this.showWeekends = showWeekends;
-    }
-
-    public boolean isSlotEventOverlap() {
-        return slotEventOverlap;
-    }
-
-    public void setSlotEventOverlap(boolean slotEventOverlap) {
-        this.slotEventOverlap = slotEventOverlap;
-    }
-
-    public boolean isShowWeekNumbers() {
-        return showWeekNumbers;
-    }
-
-    public void setShowWeekNumbers(boolean showWeekNumbers) {
-        this.showWeekNumbers = showWeekNumbers;
-    }
-
-    public boolean isShowHeader() {
-        return showHeader;
-    }
-
-    public void setShowHeader(boolean showHeader) {
-        this.showHeader = showHeader;
-    }
-
-    public boolean isDraggable() {
-        return draggable;
-    }
-
-    public void setDraggable(boolean draggable) {
-        this.draggable = draggable;
-    }
-
-    public boolean isResizable() {
-        return resizable;
-    }
-
-    public void setResizable(boolean resizable) {
-        this.resizable = resizable;
-    }
-
-    public boolean isTooltip() {
-        return tooltip;
-    }
-
-    public void setTooltip(boolean tooltip) {
-        this.tooltip = tooltip;
-    }
-
-    public boolean isRtl() {
-        return rtl;
-    }
-
-    public void setRtl(boolean rtl) {
-        this.rtl = rtl;
-    }
-
-    public boolean isAllDaySlot() {
-        return allDaySlot;
-    }
-
-    public void setAllDaySlot(boolean allDaySlot) {
-        this.allDaySlot = allDaySlot;
-    }
-
-    public double getAspectRatio() {
-        return aspectRatio == 0 ? Double.MIN_VALUE : aspectRatio;
-    }
-
-    public void setAspectRatio(double aspectRatio) {
-        this.aspectRatio = aspectRatio;
-    }
-
-    public String getLeftHeaderTemplate() {
-        return leftHeaderTemplate;
-    }
-
-    public void setLeftHeaderTemplate(String leftHeaderTemplate) {
-        this.leftHeaderTemplate = leftHeaderTemplate;
-    }
-
-    public String getCenterHeaderTemplate() {
-        return centerHeaderTemplate;
-    }
-
-    public void setCenterHeaderTemplate(String centerHeaderTemplate) {
-        this.centerHeaderTemplate = centerHeaderTemplate;
-    }
-
-    public String getRightHeaderTemplate() {
-        return rightHeaderTemplate;
-    }
-
-    public void setRightHeaderTemplate(String rightHeaderTemplate) {
-        this.rightHeaderTemplate = rightHeaderTemplate;
-    }
-
-    public String getView() {
-        return view;
-    }
-
-    public void setView(String view) {
-        this.view = view;
-    }
-
-    public String getNextDayThreshold() {
-        return nextDayThreshold;
-    }
-
-    public void setNextDayThreshold(String nextDayThreshold) {
-        this.nextDayThreshold = nextDayThreshold;
-    }
-
-    public String getWeekNumberCalculation() {
-        return weekNumberCalculation;
-    }
-
-    public void setWeekNumberCalculation(String weekNumberCalculation) {
-        this.weekNumberCalculation = weekNumberCalculation;
-    }
-
-    public String getWeekNumberCalculator() {
-        return weekNumberCalculator;
-    }
-
-    public void setWeekNumberCalculator(String weekNumberCalculator) {
-        this.weekNumberCalculator = weekNumberCalculator;
-    }
-
-    public String getTimeFormat() {
-        return timeFormat;
-    }
-
-    public void setTimeFormat(String timeFormat) {
-        this.timeFormat = timeFormat;
-    }
-
-    public String getSlotDuration() {
-        return slotDuration;
-    }
-
-    public void setSlotDuration(String slotDuration) {
-        this.slotDuration = slotDuration;
-    }
-
-    public String getSlotLabelInterval() {
-        return slotLabelInterval;
-    }
-
-    public void setSlotLabelInterval(String slotLabelInterval) {
-        this.slotLabelInterval = slotLabelInterval;
-    }
-
-    public String getSlotLabelFormat() {
-        return slotLabelFormat;
-    }
-
-    public void setSlotLabelFormat(String slotLabelFormat) {
-        this.slotLabelFormat = slotLabelFormat;
-    }
-
-    public String getDisplayEventEnd() {
-        return displayEventEnd;
-    }
-
-    public void setDisplayEventEnd(String displayEventEnd) {
-        this.displayEventEnd = displayEventEnd;
-    }
-
-    public String getScrollTime() {
-        return scrollTime;
-    }
-
-    public void setScrollTime(String scrollTime) {
-        this.scrollTime = scrollTime;
-    }
-
-    public String getMinTime() {
-        return minTime;
-    }
-
-    public void setMinTime(String minTime) {
-        this.minTime = minTime;
-    }
-
-    public String getMaxTime() {
-        return maxTime;
-    }
-
-    public void setMaxTime(String maxTime) {
-        this.maxTime = maxTime;
-    }
-
-    public String getLocale() {
-        return locale;
-    }
-
-    public void setLocale(String locale) {
-        this.locale = locale;
-    }
-
-    public String getTimeZone() {
-        return timeZone;
-    }
-
-    public void setTimeZone(String timeZone) {
-        this.timeZone = timeZone;
-    }
-
-    public String getClientTimeZone() {
-        return clientTimeZone;
-    }
-
-    public void setClientTimeZone(String clientTimeZone) {
-        this.clientTimeZone = clientTimeZone;
-    }
-
-    public String getColumnHeaderFormat() {
-        return columnHeaderFormat;
-    }
-
-    public void setColumnHeaderFormat(String columnHeaderFormat) {
-        this.columnHeaderFormat = columnHeaderFormat;
-    }
-
-    public ExtenderService.ExtenderExample getExtenderExample() {
-        return extenderExamples.get(selectedExtenderExample);
-    }
-
-    public String getSelectedExtenderExample() {
-        return selectedExtenderExample;
-    }
-
-    public void setSelectedExtenderExample(String selectedExtenderExample) {
-        this.selectedExtenderExample = selectedExtenderExample;
-    }
-
-    public String getExtenderCode() {
-        return extenderCode;
-    }
-
-    public void setExtenderCode(String extenderCode) {
-        this.extenderCode = extenderCode;
-    }
-
-    public String getHeight() {
-        return height;
-    }
-
-    public void setHeight(String height) {
-        this.height = height;
-    }
-
-    public List<SelectItem> getExtenderExamples() {
-        return extenderExamples.values().stream() //
-                .sorted(Comparator.comparing(ExtenderService.ExtenderExample::getName)) //
-                .map(example -> new SelectItem(example.getKey(), example.getName())) //
-                .collect(Collectors.toList());
-    }
 }
 
 
