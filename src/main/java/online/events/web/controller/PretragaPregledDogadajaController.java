a848390fadd1a8598512ca8b3778a8773450d972
Rudy <tomica.korpar@gmail.com>
2021-08-16 13:19:22 +0200

    kalendar svi dog

--- src/main/java/online/events/web/controller/PretragaPregledDogadajaController.java
+++ src/main/java/online/events/web/controller/PretragaPregledDogadajaController.java
@@ -10,7 +10,13 @@ import online.events.util.DogadajAppUtil;
 import org.apache.commons.lang3.StringUtils;
 import org.primefaces.component.datatable.DataTable;
 import org.primefaces.event.RowEditEvent;
+import org.primefaces.event.ScheduleEntryMoveEvent;
+import org.primefaces.event.ScheduleEntryResizeEvent;
 import org.primefaces.event.SelectEvent;
+import org.primefaces.model.DefaultScheduleEvent;
+import org.primefaces.model.DefaultScheduleModel;
+import org.primefaces.model.ScheduleEvent;
+import org.primefaces.model.ScheduleModel;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -22,6 +28,7 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -55,6 +62,51 @@ public class PretragaPregledDogadajaController implements Serializable {
     private List<OrganizacijskaJedinicaDto> organizacijskaJedinicaDtoList;
     private List<GradDto> gradDtoList;
     private List<VelicinaGradaDto> velicinaGradaDtoList;
+
+    //za kalendar
+    private ScheduleModel eventModelMyEvents;
+    private ScheduleModel eventModelAllEvents;
+    private ScheduleEvent<?> event = new DefaultScheduleEvent<>();
+
+    private List<DogadajDto> dogadajiAllList;
+
+
+    private boolean slotEventOverlap = true;
+    private boolean showWeekNumbers = false;
+    private boolean showHeader = true;
+    private boolean draggable = true;
+    private boolean resizable = true;
+    private boolean showWeekends = true;
+    private boolean tooltip = true;
+    private boolean allDaySlot = true;
+    private boolean rtl = false;
+
+    private double aspectRatio = Double.MIN_VALUE;
+
+    private String leftHeaderTemplate = "prev,next today";
+    private String centerHeaderTemplate = "title";
+    private String rightHeaderTemplate = "dayGridMonth,timeGridWeek,timeGridDay,listYear";
+    private String nextDayThreshold = "09:00:00";
+    private String weekNumberCalculation = "local";
+    private String weekNumberCalculator = "date.getTime()";
+    private String displayEventEnd;
+    private String timeFormat;
+    private String slotDuration = "00:30:00";
+    private String slotLabelInterval;
+    private String slotLabelFormat;
+    private String scrollTime = "06:00:00";
+    private String minTime = "04:00:00";
+    private String maxTime = "20:00:00";
+    private String locale = "en";
+    private String timeZone = "";
+    private String clientTimeZone = "local";
+    private String columnHeaderFormat = "";
+    private String view = "timeGridWeek";
+    private String height = "auto";
+
+    private String extenderCode = "// Write your code here or select an example from above";
+    private String selectedExtenderExample = "";
+
     //CDI
     @Inject
     private GradDao gradDao;
@@ -84,6 +136,22 @@ public class PretragaPregledDogadajaController implements Serializable {
         //initialization
         dogadajFilterDto = new DogadajFilterDto();
         dogadajFilterDto.setLoggedUser(FacesContext.getCurrentInstance().getExternalContext().getRemoteUser());
+        //calendar
+        eventModelAllEvents = new DefaultScheduleModel();
+        eventModelMyEvents = new DefaultScheduleModel();
+
+        getListAllDogadaj();
+        for (DogadajDto dogadajDto : dogadajiAllList) {
+            DefaultScheduleEvent<?> event = DefaultScheduleEvent.builder()
+                    .title(dogadajDto.getNazivDogadaja())
+                    .startDate(dogadajDto.getVrijemeOd())
+                    .endDate(dogadajDto.getVrijemeDo())
+                    .description(dogadajDto.getGradDogadajaDto().getNazivGrada())
+                    .borderColor((StringUtils.equals(dogadajDto.getSlobodanUlaz(), "DA") ? "GREEN" : "BLUE"))
+                    .overlapAllowed(true)
+                    .build();
+            eventModelAllEvents.addEvent(event);
+        }
     }
 
     /*
@@ -144,6 +212,22 @@ public class PretragaPregledDogadajaController implements Serializable {
     public void getFilterListDogadaj() {
         try {
             dogadajiFilterList = dogadajDao.getFilterList(dogadajFilterDto);
+        } catch (DogadajAppRuleException eventEx) {
+            if (eventEx.getMessages() != null && !eventEx.getMessages().isEmpty()) {
+                for (String message : eventEx.getMessages()) {
+                    eventEx.printStackTrace();
+                    addMessage(message, DogadajAppConstants.SEVERITY_ERR);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            addMessage("Došlo je do greške prilikom pretraživanja događaja.", DogadajAppConstants.SEVERITY_ERR);
+        }
+    }
+
+    public void getListAllDogadaj() {
+        try {
+            dogadajiAllList = dogadajDao.getFilterList(new DogadajFilterDto());
         } catch (DogadajAppRuleException eventEx) {
             if (eventEx.getMessages() != null && !eventEx.getMessages().isEmpty()) {
                 for (String message : eventEx.getMessages()) {
@@ -349,6 +433,48 @@ public class PretragaPregledDogadajaController implements Serializable {
         }
     }
 
+    public void getListDogadaj() {
+        try {
+            dogadajiFilterList = dogadajDao.getFilterList(dogadajFilterDto);
+        } catch (DogadajAppRuleException eventEx) {
+            if (eventEx.getMessages() != null && !eventEx.getMessages().isEmpty()) {
+                for (String message : eventEx.getMessages()) {
+                    eventEx.printStackTrace();
+                    addMessage(message, DogadajAppConstants.SEVERITY_ERR);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            addMessage("Došlo je do greške prilikom pretraživanja događaja.", DogadajAppConstants.SEVERITY_ERR);
+        }
+    }
+
+
+    public void onDateSelect(SelectEvent<LocalDateTime> selectEvent) {
+        event = DefaultScheduleEvent.builder().startDate(selectEvent.getObject()).endDate(selectEvent.getObject().plusHours(1)).build();
+    }
+
+    public void onEventSelect(SelectEvent<ScheduleEvent<?>> selectEvent) {
+        event = selectEvent.getObject();
+    }
+
+    public void onEventMove(ScheduleEntryMoveEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Delta:" + event.getDeltaAsDuration());
+
+        addMessage(message);
+    }
+
+    public void onEventResize(ScheduleEntryResizeEvent event) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Start-Delta:" + event.getDeltaStartAsDuration() + ", End-Delta: " + event.getDeltaEndAsDuration());
+
+        addMessage(message);
+    }
+
+    private void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+
     //getters & setters
     public List<DogadajDto> getDogadajiFilterList() {
         return dogadajiFilterList;
@@ -452,5 +578,29 @@ public class PretragaPregledDogadajaController implements Serializable {
 
     public void setKalendarFilterSelectItems(List<SelectItem> kalendarFilterSelectItems) {
         this.kalendarFilterSelectItems = kalendarFilterSelectItems;
+    }
+
+    public ScheduleModel getEventModelMyEvents() {
+        return eventModelMyEvents;
+    }
+
+    public void setEventModelMyEvents(ScheduleModel eventModelMyEvents) {
+        this.eventModelMyEvents = eventModelMyEvents;
+    }
+
+    public ScheduleEvent<?> getEvent() {
+        return event;
+    }
+
+    public void setEvent(ScheduleEvent<?> event) {
+        this.event = event;
+    }
+
+    public ScheduleModel getEventModelAllEvents() {
+        return eventModelAllEvents;
+    }
+
+    public void setEventModelAllEvents(ScheduleModel eventModelAllEvents) {
+        this.eventModelAllEvents = eventModelAllEvents;
     }
 }
